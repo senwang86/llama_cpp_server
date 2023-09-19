3bdd89736884bcd23c8e1911f52bf8aab2a57173
Sen Wang <senwang@Sens-MBP-M2Pro.local>
2023-09-19 11:11:19 -0700

    Fix index.js

--- index.js
+++ index.js
@@ -1,14 +1,25 @@
-const prompt = `Building a website can be done in 10 simple steps:`;
+const prompt = `Implement binary search in Python`;
 
 async function Test() {
-    let response = await fetch("http://127.0.0.1:9090/completion", {
+    let response = await fetch("http://100.71.162.51:9090/completion", {
         method: 'POST',
         body: JSON.stringify({
             prompt,
-            n_predict: 512,
-        })
+            temperature: 0.1,
+            top_k: 40,
+            top_p: 0.9,
+            repeat_penalty: 1.05,
+            // large n_predict significantly slows down the server, thus use a small value for testing purposes
+            n_predict: 32,
+            stream: false,
+        }),
+        headers: {
+            'Content-Type': 'application/json'
+          }
     })
-    console.log((await response.json()).content)
+    resJSON = JSON.parse((await response.text()));
+    console.log(resJSON);
+    return resJSON['content'];
 }
 
 Test()
